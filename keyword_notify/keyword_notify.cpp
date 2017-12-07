04153f95e544efff456fde2a493b9f76856e106c
ysouyno <ysouyno@163.com>
2017-12-07 21:52:27 +0800

    Replaces std::vector with a combination of std::map and std::list.

--- keyword_notify/keyword_notify.cpp
+++ keyword_notify/keyword_notify.cpp
@@ -2,13 +2,17 @@
 #include <tchar.h>
 #include <string>
 #include <io.h> // for _taccess_s
-#include <vector>
+#include <map>
+#include <list>
 
 #ifdef UNICODE
 typedef std::wstring tstring;
 #else
 typedef std::string tstring;
 #endif
+
+const size_t max_files_count = 8;
+const size_t max_keywords_count = 8;
 
 bool get_config_file_name(tstring &config_file)
 {
@@ -64,150 +68,63 @@ void gen_config_template()
 	}
 
 	_ftprintf_s(fp, _T("%s\n"), _T(";;for example"));
-	_ftprintf_s(fp, _T("%s\n"), _T("; [log_path]"));
-	_ftprintf_s(fp, _T("%s\n"), _T("; path1=C:\\log.log"));
-	_ftprintf_s(fp, _T("%s\n"), _T("; path2=D:\\log.log"));
-	_ftprintf_s(fp, _T("%s\n"), _T("; [keyword_list]"));
+	_ftprintf_s(fp, _T("%s\n"), _T(""));
+	_ftprintf_s(fp, _T("%s\n"), _T("; [target_file1]"));
+	_ftprintf_s(fp, _T("%s\n"), _T("; path=C:\\log.log"));
 	_ftprintf_s(fp, _T("%s\n"), _T("; keyword1=error1"));
 	_ftprintf_s(fp, _T("%s\n"), _T("; keyword2=error2"));
 	_ftprintf_s(fp, _T("%s\n"), _T(""));
-	_ftprintf_s(fp, _T("%s\n"), _T("[log_path]"));
-	_ftprintf_s(fp, _T("%s\n"), _T("path1="));
-	_ftprintf_s(fp, _T("%s\n"), _T("[keyword_list]"));
-	_ftprintf_s(fp, _T("%s\n"), _T("keyword1="));
+	_ftprintf_s(fp, _T("%s\n"), _T("; [target_file2]"));
+	_ftprintf_s(fp, _T("%s\n"), _T("; path=D:\\log.log"));
+	_ftprintf_s(fp, _T("%s\n"), _T("; keyword1=warn1"));
 
 	fclose(fp);
 }
 
-void query_info_from_config(tstring &path, tstring &keyword)
+void query_info_from_config(std::map<tstring, std::list<tstring> > &info_map)
 {
 	tstring config;
 	get_config_file_name(config);
 
+	TCHAR app_name[_MAX_FNAME] = { 0 };
 	TCHAR temp_path[_MAX_PATH] = { 0 };
-	TCHAR temp_keyword[_MAX_PATH] = { 0 };
-	GetPrivateProfileString(_T("log_path"), _T("path"), NULL, temp_path, _MAX_PATH, config.c_str());
-	GetPrivateProfileString(_T("keyword_list"), _T("keyword"), NULL, temp_keyword, _MAX_PATH, config.c_str());
-
-	path.erase();
-	keyword.erase();
-
-	path += temp_path;
-	keyword += temp_keyword;
-}
-
-void query_info_from_config(std::vector<tstring> &path_vec, std::vector<tstring> &keyword_vec)
-{
-	tstring config;
-	get_config_file_name(config);
-
-	TCHAR key_name[_MAX_FNAME] = { 0 };
-	TCHAR temp_path[_MAX_PATH] = { 0 };
-	for (int i = 0; i < 64; ++i)
+	for (int i = 0; i < max_files_count; ++i)
 	{
-		_stprintf_s(key_name, _T("path%d"), i + 1);
+		_stprintf_s(app_name, _T("target_file%d"), i + 1);
 
-		DWORD ret = GetPrivateProfileString(_T("log_path"), key_name, NULL, temp_path, _MAX_PATH, config.c_str());
+		DWORD ret = GetPrivateProfileString(app_name, _T("path"), NULL, temp_path, _MAX_PATH, config.c_str());
 		if (0 == ret)
 			break;
 
-		tstring str(temp_path);
-		path_vec.push_back(str);
-	}
+		std::list<tstring> keyword_list;
 
-	TCHAR temp_keyword[_MAX_PATH] = { 0 };
-	for (int i = 0; i < 64; ++i)
-	{
-		_stprintf_s(key_name, _T("keyword%d"), i + 1);
-
-		DWORD ret = GetPrivateProfileString(_T("keyword_list"), key_name, NULL, temp_keyword, _MAX_PATH, config.c_str());
-		if (0 == ret)
-			break;
-
-		tstring str(temp_keyword);
-		keyword_vec.push_back(str);
-	}
-}
-
-void notify_keyword(const tstring &path, const tstring &keyword)
-{
-	tstring target_dir;
-	target_dir = path.substr(0, path.find_last_of(_T('\\')));
-
-	struct _stat last_stat = { 0 };
-	_tstat(path.c_str(), &last_stat);
-
-	_off_t file_size = last_stat.st_size;
-
-	HANDLE h = INVALID_HANDLE_VALUE;
-	h = FindFirstChangeNotification(target_dir.c_str(), FALSE, FILE_NOTIFY_CHANGE_SIZE);
-	if (INVALID_HANDLE_VALUE == h)
-	{
-		_tprintf(_T("FindFirstChangeNotification error: %d\n"), GetLastError());
-		return;
-	}
-
-	while (true)
-	{
-		WaitForSingleObject(h, INFINITE);
-
-		// need to sleep before call _tstat, or open the file will fail
-		Sleep(50);
-
-		struct _stat new_stat = { 0 };
-		_tstat(path.c_str(), &new_stat);
-
-		if (file_size != new_stat.st_size)
+		TCHAR key_name[_MAX_FNAME] = { 0 };
+		TCHAR temp_key[_MAX_FNAME] = { 0 };
+		for (int j = 0; j < max_keywords_count; ++j)
 		{
-			_tprintf(_T("%s changed, new size: %d\n"), path.c_str(), new_stat.st_size);
+			_stprintf_s(key_name, _T("keyword%d"), j + 1);
 
-			FILE *fp = NULL;
-			errno_t err = 0;
-			err = _tfopen_s(&fp, path.c_str(), _T("r"));
-			if (0 != err)
-			{
-				// why sometimes error 13 (Permission denied)?
-				_tprintf(_T("_tfopen_s error: %d\n"), err);
-				continue;
-			}
+			DWORD ret = GetPrivateProfileString(app_name, key_name, NULL, temp_key, _MAX_FNAME, config.c_str());
+			if (0 == ret)
+				break;
 
-			fseek(fp, file_size, SEEK_SET);
-
-			TCHAR buff[1024] = { 0 };
-
-			while (!feof(fp))
-			{
-				_fgetts(buff, 1024, fp);
-
-				if (_tcsstr(buff, keyword.c_str()))
-					_tprintf(_T("%s\n"), buff);
-			}
-
-			fclose(fp);
-			file_size = new_stat.st_size;
+			tstring str(temp_key);
+			keyword_list.push_back(str);
 		}
 
-		if (FALSE == FindNextChangeNotification(h))
-		{
-			_tprintf(_T("FindNextChangeNotification error: %d\n"), GetLastError());
-			break;
-		}
+		info_map.insert(std::pair<tstring, std::list<tstring> >(temp_path, keyword_list));
 	}
-
-	FindCloseChangeNotification(h);
-	return;
 }
 
 typedef struct _thread_param
 {
 	tstring path;
-	tstring keyword;
+	std::list<tstring> keywords_list;
 } thread_param, *pthread_param;
 
 DWORD WINAPI thread_proc(LPVOID param)
 {
 	pthread_param pp = (pthread_param)param;
-	_tprintf(_T("%s, %s\n"), pp->path.c_str(), pp->keyword.c_str());
 
 	tstring target_dir;
 	target_dir = pp->path.substr(0, pp->path.find_last_of(_T('\\')));
@@ -257,8 +174,11 @@ DWORD WINAPI thread_proc(LPVOID param)
 			{
 				_fgetts(buff, 1024, fp);
 
-				if (_tcsstr(buff, pp->keyword.c_str()))
-					_tprintf(_T("%s\n"), buff);
+				for (std::list<tstring>::iterator iter = pp->keywords_list.begin(); iter != pp->keywords_list.end(); ++iter)
+				{
+					if (_tcsstr(buff, iter->c_str()))
+						_tprintf(_T("%s\n"), buff);
+				}
 			}
 
 			fclose(fp);
@@ -277,20 +197,17 @@ DWORD WINAPI thread_proc(LPVOID param)
 	return 0;
 }
 
-void notify_keyword(const std::vector<tstring> &path_vec, const std::vector<tstring> &keyword_vec)
+void notify_keyword(const std::map<tstring, std::list<tstring> > &info_map)
 {
-	for (std::vector<tstring>::const_iterator path_citer = path_vec.cbegin(); path_citer != path_vec.cend(); ++path_citer)
+	for (std::map<tstring, std::list<tstring> >::const_iterator citer = info_map.cbegin(); citer != info_map.cend(); ++citer)
 	{
-		for (std::vector<tstring>::const_iterator keyword_citer = keyword_vec.cbegin(); keyword_citer != keyword_vec.cend(); ++keyword_citer)
-		{
-			thread_param *ptp = new thread_param;
-			ptp->path = *path_citer;
-			ptp->keyword = *keyword_citer;
+		thread_param *ptp = new thread_param;
+		ptp->path = citer->first;
+		ptp->keywords_list = citer->second;
 
-			HANDLE h = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread_proc, ptp, 0, NULL);
-			if (NULL != h)
-				CloseHandle(h);
-		}
+		HANDLE h = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread_proc, ptp, 0, NULL);
+		if (NULL != h)
+			CloseHandle(h);
 	}
 }
 
@@ -308,23 +225,11 @@ int main()
 		return -1;
 	}
 
-	/*tstring path;
-	tstring keyword;
-	query_info_from_config(path, keyword);
-	if (path.empty() || keyword.empty())
-	{
-		_tprintf(_T("path or keyword in config file is empty\n"));
-		return -1;
-	}*/
+	std::map<tstring, std::list<tstring> > info_map;
+	query_info_from_config(info_map);
 
-	std::vector<tstring> path_vec;
-	std::vector<tstring> keyword_vec;
-	query_info_from_config(path_vec, keyword_vec);
-
-	/*notify_keyword(path, keyword);*/
-	notify_keyword(path_vec, keyword_vec);
-
-	system("pause");
+	notify_keyword(info_map);
+	getchar();
 
 	return 0;
 }
